1277754694b0aa11236da53db18537244d6e5c4c
Robert Wikman <rbw@vault13.org>
2019-08-11 20:14:47 +0200

    Rework root CLI
    Signed-off-by: Robert Wikman <rbw@vault13.org>

--- aioli/commands/root/commands.py
+++ aioli/commands/root/commands.py
@@ -4,34 +4,36 @@ from uvicorn import run as run_server, importer
 
 
 @click.group(invoke_without_command=False)
-def root_group():
-    pass
+@click.pass_context
+def root_group(ctx):
+    if ctx.obj is None:
+        ctx.obj = {}
 
 
-@click.command(name="dev-start", help="Start development server")
+@click.command("start", help="Start the development server")
 @click.option("--host", help="Bind socket to this host", default="127.0.0.1", show_default=True, type=str)
 @click.option("--port",  help="Bind socket to this port", default="5000", show_default=True, type=int)
 @click.option("--no_reload", help="Disable reloader", default=False, type=bool)
 @click.option("--no_debug", help="Disable debug mode", default=False, type=bool)
 @click.option("--workers", help="Number of workers", show_default=True, default=1, type=int)
 @click.option(
-    "--path",
+    "--app",
     help="Example: app=Application(...) in my_proj/__main__.py becomes --path my_proj:app",
     required=True,
     show_default=True,
     type=str
 )
-def dev_start(path, host, port, **kwargs):
+def dev_start(app_path, host, port, **kwargs):
     kwargs["reload"] = not kwargs.pop("no_reload")
     kwargs["debug"] = not kwargs.pop("no_debug")
 
-    config = importer.import_from_string(path).config
+    config = importer.import_from_string(app_path).config
 
     run_server(
-        path,
+        app_path,
         host=host or config["dev_host"],
         port=port or config["dev_port"],
         loop="uvloop",
         log_level="debug" if kwargs["debug"] else "info",
-        **kwargs
+        workers=kwargs.pop("workers", 1)
     )
